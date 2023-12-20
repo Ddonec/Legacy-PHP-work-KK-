c9cbaf61a7694227da23173fcc9fd832807302b4
Anton <137609245+Ddonec@users.noreply.github.com>
2023-12-20 22:37:21 +0300

    feat: footer update PHP legacy

--- Kyti-katai/footer.php
+++ Kyti-katai/footer.php
@@ -3,6 +3,10 @@
 
 
 
+
+
+
+
 <footer>
          <div class="images-footer">
             <img src="<?php echo bloginfo('template_url'); ?>/assets/assets/icon/Group3.svg" alt="" />
@@ -10,23 +14,28 @@
                <img class="pointer" src="<?php echo bloginfo('template_url'); ?>/assets/assets/content/appstore.png" alt="" />
                <img class="pointer" src="<?php echo bloginfo('template_url'); ?>/assets/assets/content/googleplay.png" alt="" />
             </div>
+
+            <div class="social-circles-footer-index">
+               <img class="pointer" src="<?php echo bloginfo('template_url'); ?>/assets/assets/icon/socials.svg" alt="" />
+               <img class="pointer" src="<?php echo bloginfo('template_url'); ?>/assets/assets/icon/tg.svg" alt="" />
+            </div>
          </div>
          <div class="columns-footer">
             <div class="row-footer-1">
                <div class="title-list-1 poinetr">Компания</div>
                <ul>
-                  <li class="pointer"><a href="<?php echo get_option('home'); ?>/item">Техника</a></li>
-                  <li class="pointer"><a href="<?php echo get_option('home'); ?>/find">Мы в парках</a></li>
-                  <li class="pointer"><a href="<?php echo get_option('home'); ?>/about-us">О нас</a></li>
-                  <li class="pointer"><a href="<?php echo get_option('home'); ?>/news-1">Новости</a></li>
-                  <li class="pointer"><a href="<?php echo get_option('home'); ?>/work-in">Работа у нас</a></li>
+                  <li class="pointer"><a href="<?php echo get_option('home'); ?>/item.html">Техника</a></li>
+                  <li class="pointer"><a href="<?php echo get_option('home'); ?>/find.html">Мы в парках</a></li>
+                  <li class="pointer"><a href="<?php echo get_option('home'); ?>/about-us.html">О нас</a></li>
+                  <li class="pointer"><a href="<?php echo get_option('home'); ?>/news-1.html">Новости</a></li>
+                  <li class="pointer"><a href="<?php echo get_option('home'); ?>/work-in.html">Работа у нас</a></li>
                </ul>
             </div>
 
             <div class="row-footer-1">
                <div class="title-list-1 pointer">Сервис</div>
                <ul>
-                  <li class="pointer"><a href="<?php echo get_option('home'); ?>/repair">Ремонт техники</a></li>
+                  <li class="pointer"><a href="<?php echo get_option('home'); ?>/repair.html">Ремонт техники</a></li>
                   <li class="pointer">Сезонное хранение</li>
                   <li class="pointer">Велотур</li>
                   <li class="pointer">Корп. клиентам</li>
@@ -37,10 +46,10 @@
             <div class="row-footer-2">
                <div class="title-list-1 pointer">Документация</div>
                <ul>
-                  <li class="pointer"><a class="white-text-section" href="<?php echo get_option('home'); ?>/rules">Правила проката</a></li>
-                  <li class="pointer"><a class="white-text-section" href="<?php echo get_option('home'); ?>/oferta">Договор оферта</a></li>
-                  <li class="pointer"><a class="white-text-section" href="<?php echo get_option('home'); ?>/privacy">Политика конфиденциальности</a></li>
-                  <li class="pointer"><a class="white-text-section" href="<?php echo get_option('home'); ?>/app-agreement">Пользовательское соглашение для мобильного приложения</a></li>
+                  <li class="pointer"><a class="white-text-section" href="<?php echo get_option('home'); ?>/rules.html">Правила проката</a></li>
+                  <li class="pointer"><a class="white-text-section" href="<?php echo get_option('home'); ?>/oferta.html">Договор оферта</a></li>
+                  <li class="pointer"><a class="white-text-section" href="<?php echo get_option('home'); ?>/privacy-politic.html">Политика конфиденциальности</a></li>
+                  <li class="pointer"><a class="white-text-section" href="<?php echo get_option('home'); ?>/app-agreement.html">Пользовательское соглашение для мобильного приложения</a></li>
                </ul>
             </div>
             <div class="apps-links-footer-">
@@ -48,7 +57,7 @@
                <img class="pointer" src="<?php echo bloginfo('template_url'); ?>/assets/assets/content/googleplay.png" alt="" />
             </div>
             <div class="row-footer-1">
-               <a href="<?php echo get_option('home'); ?>/contacts"><div class="title-list-1">Контакты</div></a>
+               <a href="<?php echo get_option('home'); ?>/contacts.html"><div class="title-list-1">Контакты</div></a>
                <ul>
                   <li>
                      <span class="yellow-number-footer pointer"><a id="yellow-tel-numb-footer" href="tel:+79254297029">+7(925) 429-70-29</a></span>
@@ -69,10 +78,8 @@
          </div>
       </footer>
    </body>
-   <script src="js/index.js"></script>
-   <script src="js/catalog.js"></script>
-
 </html>
+
 
 
 
