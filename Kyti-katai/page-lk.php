c405340e5343832af2f07fef3afaf7316cff6357
Anton <137609245+Ddonec@users.noreply.github.com>
2024-02-01 03:18:37 +0300

    feat: LK full rdy to WP

--- Kyti-katai/page-lk.php
+++ Kyti-katai/page-lk.php
@@ -23,10 +23,10 @@ get_header();
             <p>/</p>
             <p class="grey-bread-crumbs">Личный кабинет</p>
          </div>
-         <h1 class="h1-no-main-page text-gradient cabinet-title">Личный кабинет</h1>
+         <h1 class="h1-no-main-page text-gradient cabinet-title"><?php the_field('zagolovok_straniczy') ?></h1>
          <main class="cabinet-content-container text-14-500-left-lato">
             <div class="user-data-cabinet">
-               <div class="user-title-cabinet mons-28-700">Ваши данные</div>
+               <div class="user-title-cabinet mons-28-700"><?php the_field('lichnye_dannye') ?></div>
                <div class="user-data-cabinet__discription">
                   <div>
                      <p class="user-data-cabinet__data-title opacity">Ваше имя</p>
@@ -49,28 +49,28 @@ get_header();
             <!-- <div class="cabinet-verification-container text-14-500-left-lato"> -->
             <div class="cabinet-go-check">
                <div class="cabinet-go-check__verif-info">
-                  <p class="cabinet-go-check__verif-title mons-28-700">Пройдите верификацию</p>
+                  <p class="cabinet-go-check__verif-title mons-28-700"><?php the_field('verefikacziya') ?></p>
                   <p class="cabinet-go-check__verif-subtitle"><span class="phone-verif-cabinet">8 902 510 54 73</span> ваш номер телефона?</p>
                </div>
                <div class="cabinet-go-check__buttons-zone">
                   <div class="cabinet-go-check__btn-1">Да, всё верно</div>
                   <div class="cabinet-go-check__btn-2">Ввести другой</div>
                   <div class="cabinet-go-check__btn-3">
-                     <img src="assets/content/gosuslugi.png" alt="" />
+                     <img src="<?php echo esc_url(get_field('gosuslugi')); ?>" alt="" />
                      <p>Госулуги</p>
                   </div>
                   <div class="cabinet-go-check__btn-4">
-                     <img src="assets/icon/sber.svg" alt="" />
+                     <img src="<?php echo bloginfo('template_url'); ?>/assets/assets/icon/sber.svg" alt="" />
                      <p>ID</p>
                   </div>
                   <div class="cabinet-go-check__btn-5">
-                     <img src="assets/content/tinkoff.png" alt="" />
+                     <img src="<?php echo esc_url(get_field('tinkoff')); ?>" alt="" />
                      <p>ID</p>
                   </div>
                </div>
-               <img class="image-blue-check-cabinet" src="assets/content/image-blue-check-cabinet.png" alt="" />
+               <img class="image-blue-check-cabinet" src="<?php echo esc_url(get_field('galochka_logo')); ?>" alt="" />
             </div>
-            <div class="cabinet-orders mons-28-700">Заказы</div>
+            <div class="cabinet-orders mons-28-700"><?php the_field('orders') ?></div>
             <!-- </div> -->
          </main>
       </section>
