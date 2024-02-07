30c3d7db490365a4c2b6b1f3f3b586d7b43d5271
Alex Tretyakov <alex@tretyakov.net>
2024-02-07 09:23:51 +0300

    Добавил файлы для вывода товаров и категорий

---
+++ Kyti-katai/sidebar-shop.php
@@ -0,0 +1,49 @@
+<section class="catalog-filter-section">
+    <div class="filter-container-catalog">
+        <div class="catalog-input-container">
+            <p>Стоимость аренды</p>
+            <label for="input-min"></label>
+            <input type="number" id="input-min" min="300" max="50000" step="1" />
+
+            <label for="input-max"></label>
+            <input type="number" id="input-max" min="300" max="50000" step="1" />
+            <div id="price-values">
+                <span id="price-min"></span>
+                <span id="price-max"></span>
+            </div>
+            <div id="price-slider"></div>
+        </div>
+    </div>
+    <div class="checkbox-container_catalog">
+        <p>Производители</p>
+        <div><input type="checkbox" /><label for="">Format</label></div>
+        <div><input type="checkbox" /><label for="">Aspect</label></div>
+        <div><input type="checkbox" /><label for="">BBB</label></div>
+        <div><input type="checkbox" /><label for="">Hamax</label></div>
+        <div><input type="checkbox" /><label for="">SKS</label></div>
+        <div class="show-more-filter-catalog">+ Показать еще</div>
+    </div>
+    <div class="checkbox-container_catalog">
+        <p>Диаметр колеса</p>
+        <div><input type="checkbox" /><label for="">8”</label></div>
+        <div><input type="checkbox" /><label for="">12”</label></div>
+        <div><input type="checkbox" /><label for="">12,5”</label></div>
+        <div><input type="checkbox" /><label for="">14”</label></div>
+        <div><input type="checkbox" /><label for="">16”</label></div>
+        <div><input type="checkbox" /><label for="">18”</label></div>
+        <div><input type="checkbox" /><label for="">20”</label></div>
+        <div><input type="checkbox" /><label for="">21”</label></div>
+        <div><input type="checkbox" /><label for="">24”</label></div>
+        <div class="show-more-filter-catalog">- Скрыть</div>
+    </div>
+    <div class="checkbox-container_catalog">
+        <p>Цвет</p>
+        <div><input type="checkbox" /><label for="">Синий</label></div>
+        <div><input type="checkbox" /><label for="">Зеленый</label></div>
+        <div><input type="checkbox" /><label for="">Фиолетовый</label></div>
+        <div><input type="checkbox" /><label for="">Белый</label></div>
+        <div><input type="checkbox" /><label for="">Красный</label></div>
+        <div class="show-more-filter-catalog">+ Показать еще</div>
+    </div>
+    <div class="button-reset-filter-catalog-page">Сбросить фильтры</div>
+</section>
