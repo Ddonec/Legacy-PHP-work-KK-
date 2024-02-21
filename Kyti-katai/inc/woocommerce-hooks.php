15f330e8880ec6d5f22ba2b882c25cc51299d680
Alex Tretyakov <alex@tretyakov.net>
2024-02-21 13:07:32 +0300

    Добавил учет Всесезона и парка при выводе товаров

--- Kyti-katai/inc/woocommerce-hooks.php
+++ Kyti-katai/inc/woocommerce-hooks.php
@@ -15,6 +15,19 @@ add_filter( 'woocommerce_breadcrumb_defaults', function () {
 remove_action( 'woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10 );
 remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5 );
 
+//устанавливаем парк глобально
+global $parkName;
+
+// Получаем значение куки с именем 'parkData' и выделяем от туда имя парка
+$cookie_value = isset($_COOKIE['parkData']) ? $_COOKIE['parkData'] : '';
+$decoded_str = stripslashes($cookie_value);
+
+$data = json_decode($decoded_str, true);
+
+if ($data && isset($data['name'])) {
+    $parkName= $data['name'];
+}
+
 
 //устанавливаем сезон глобально
 global $season;
@@ -25,8 +38,10 @@ if ($_COOKIE['SummerWinterCheckStatus'] == 'true') {
 }
 
 
-//получаем родительские категории где есть товары с учетом сезона
-function get_parent_product_categories_season($season) {
+//получаем родительские категории где есть товары с учетом сезона и парка
+function get_parent_product_categories_season() {
+    global $season;
+    global $parkName;
     // Получаем все термины (категории) с типом 'product_cat'
     $terms = get_terms( array(
         'taxonomy'   => 'product_cat',
@@ -39,17 +54,26 @@ function get_parent_product_categories_season($season) {
     // Перебираем все категории
     foreach ( $terms as $term ) {
         // Получаем все товары в текущей категории с заданным тегом
+
+        $tax_query[] = array(
+            'taxonomy' => 'product_tag',
+            'field' => 'slug',
+            'terms' => array( 'vsesezon', $season ),
+        );
+
+        if ($parkName) {
+            $tax_query[] = array(
+                'taxonomy' => 'point',
+                'field' => 'name',
+                'terms' => $parkName,
+            );
+        }
+
         $args = array(
             'post_type'      => 'product',
             'posts_per_page' => 1,
             'product_cat'    => $term->slug,
-            'tax_query'      => array(
-                array(
-                    'taxonomy' => 'product_tag',
-                    'field' => 'slug',
-                    'terms' => $season
-                ),
-            ),
+            'tax_query'      => $tax_query,
         );
 
         $products = new WP_Query( $args );
@@ -65,18 +89,28 @@ function get_parent_product_categories_season($season) {
 }
 
 
-// изменяем стандартный вывод товаров с учетом сезона
+// изменяем стандартный вывод товаров с учетом сезона и парка
 add_action( 'woocommerce_product_query', 'custom_product_query' );
 function custom_product_query( $q ){
     global $season;
+    global $parkName;
 
     $tax_query = (array) $q->get( 'tax_query' );
 
     $tax_query[] = array(
         'taxonomy' => 'product_tag',
         'field' => 'slug',
-        'terms' => $season,
+        'terms' => array( 'vsesezon', $season ),
     );
+
+    if ($parkName) {
+        $tax_query[] = array(
+            'taxonomy' => 'point',
+            'field' => 'name',
+            'terms' => $parkName,
+        );
+    }
+
 
     $q->set( 'tax_query', $tax_query );
 }
